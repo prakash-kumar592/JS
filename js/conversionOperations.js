f96998949e73628f4cda100b1fe54d9d06363f6f
prakash-kumar592 <prakash.kumar989810@gmail.com>
2025-12-13 14:13:04 +0000

    conversion of data type

---
+++ js/conversionOperations.js
@@ -0,0 +1,32 @@
+//let score = "25"
+//let score = "25abc"
+//let score = null
+//let score = undefined
+//let score = true
+
+// console.log(typeof score);
+// console.log(typeof(score));//both will be correct to check which data type
+
+// let valueinNumber = Number(score)
+// console.log(typeof valueinNumber);
+
+// console.log(valueinNumber);
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1
+//false => 0
+
+// let isLoggedIn = 1
+// let booleanisLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanisLoggedIn);
+//1 => true
+//0 => false
+//""=> empty string => false
+//"prakash" => true
+
+let someNumber = 44
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber);

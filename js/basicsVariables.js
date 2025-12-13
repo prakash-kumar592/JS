d1aa5e21e8e75be26f5316e3b90b7bf9f511f6e4
prakash-kumar592 <prakash.kumar989810@gmail.com>
2025-12-13 08:45:25 +0000

    datatypes

---
+++ js/basicsVariables.js
@@ -0,0 +1,13 @@
+const accountId = 14545
+let accountEmail = "prakash@gmail.com"
+var accountPass = "142553"
+accountCity = "jaipur"
+let accountState;
+
+//accoutnId = 2//not allowed
+accountEmail = "prmd@gmail.com"
+accountPass = "73871"
+accountCity = "patna"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPass,accountCity,accountState])
+
---
+++ js/dataTypes.js
@@ -0,0 +1,19 @@
+"use strict";// treat all js code as newer version
+
+//alert(3 + 3)// we are using node js ,not browser
+console.log(3 +3)
+
+let name = "prakash"//string data type
+let age = 20//int data type
+let flag = false//boolean data type
+
+//null => standalone value
+//undefined =>means value is not assign
+//symbol => for unique
+
+//object
+
+console.log(typeof "prakash");//used to check data type
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);

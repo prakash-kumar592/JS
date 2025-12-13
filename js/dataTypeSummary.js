fb167e7824ac36a3a5f1307d44facc0ce24f2f53
prakash-kumar592 <prakash.kumar989810@gmail.com>
2025-12-13 16:48:59 +0000

    data type summary

---
+++ js/dataTypeSummary.js
@@ -0,0 +1,24 @@
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id == anotherId)//they gives false
+
+// non primitive
+
+const heros = ["shaktiman", "naagraj", "doga"]
+let myobj = {
+    name : "prakash",
+    age : 20,
+}
+
+const myFunction = function(){
+    console.log("hello world")
+}
+

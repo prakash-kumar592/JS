c648e4cc89618f5e9329b05a1638e6f1e96f4766
prakash-kumar592 <prakash.kumar989810@gmail.com>
2025-12-14 19:03:58 +0000

    string basics

---
+++ js/Strings.js
@@ -0,0 +1,38 @@
+const name = "prakash"
+const repocount = 50
+
+//console.log(name + repocount + "value")
+
+//console.log(`hello my name is ${name} and my repo count is ${repocount}`)
+
+//const gameName = new String('prakash')//2nd method to represent string
+//console.log(gameName[0]);
+
+//console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(3));
+//console.log(gameName.indexOf('k'));
+
+// const newstring = gameName.substring(0,4)
+// console.log(newstring)
+
+// const anotherstring = gameName.slice(-7,4)
+// console.log(anotherstring)
+
+// const newStringOne = "     pra  kash    "
+// console.log(newStringOne)
+// console.log(newStringOne.trim())//remove the spaces form start and end
+// console.log(newStringOne.trimStart())//remove the spaces form start
+// console.log(newStringOne.trimEnd())//remove the spaces form end
+
+// const url = "https://www.google.com/prakash%20kumar"
+// console.log(url.replace("%20","_"))
+
+// console.log(url.includes("prakash"))//to check whether this string is present in this url or not
+
+const gameName = new String('prakash kumar ashish')
+console.log(gameName.split(" "))//separate on the basis of which meana - or apace 
+
+
+

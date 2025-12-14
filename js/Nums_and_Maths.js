624534ca45f79859f9a13a9b02db61e196e1d589
prakash-kumar592 <prakash.kumar989810@gmail.com>
2025-12-14 19:03:36 +0000

    number qand maths

---
+++ js/Nums_and_Maths.js
@@ -0,0 +1,41 @@
+// const score = 400
+// console.log(score)
+
+//const balance = new Number(100)
+// console.log(balance)
+
+// console.log(balance.toString())
+// console.log(balance.toString().length)
+
+//console.log(balance.toFixed(2))//how many digits after decimal i will fixed
+
+// const otherNumber = 23.474
+// console.log(otherNumber.toPrecision(4))
+
+//*************Math************
+// const num = -4
+// console.log(Math.abs(num))
+// console.log(Math)
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.4))
+// console.log(Math.ceil(4.2))//it choose only top of the value
+// console.log(Math.floor(4.8))//it choose only lowest value
+
+//console.log(Math.min(4,8,2,5,7))
+//console.log(Math.max(4,8,2,5,7))
+
+console.log(Math.random())//it gives value between 0 to 1
+console.log((Math.random()*10) + 1)//it gives value between 1 to 10
+console.log(Math.floor(Math.random()*10) + 1)//it gives value between 1 to 10 but integer
+
+//but if i want give the number between min to max
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+
+
+
+

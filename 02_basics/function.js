e3062e3763719964e09b99192a687b9985965858
prakash-kumar592 <prakash.kumar989810@gmail.com>
2025-12-18 16:43:37 +0000

    basics functions

---
+++ 02_basics/function.js
@@ -0,0 +1,37 @@
+// function myname(){
+//     console.log("p")
+//     console.log("r")
+//     console.log("a")
+//     console.log("k")
+//     console.log("a")
+//     console.log("s")
+//     console.log("h")
+// }
+// myname()
+
+// function addTwoNumbers(num1, num2){
+//    console.log(num1 + num2)
+// }
+
+function addTwoNumbers(num1, num2){
+   let result = num1 + num2
+   return result
+}
+
+const result = addTwoNumbers(3,4)
+//console.log("result:", result)
+
+// function loginUserMessage(username){
+//    if(username===undefined){
+//       console.log("please enter a username")
+//       return 
+//    }
+//    return `${username} just logged in`
+// }
+// console.log(loginUserMessage("prakash"))
+
+// function loginUserMessage(username="sam"){
+//    return `${username} just logged in`
+// }
+// console.log(loginUserMessage())//without any passing any argument
+

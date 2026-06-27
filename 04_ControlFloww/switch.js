b7fcd22a42c5a8e892c05c7cb766c9c0e680a56b
prakash-kumar592 <prakash.kumar989810@gmail.com>
2026-06-27 03:43:06 +0000

    conditional statement

---
+++ 04_ControlFloww/switch.js
@@ -0,0 +1,29 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");//yadi ham break use na kre toh uske niche ka sare code ko execute kr deta hai
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}

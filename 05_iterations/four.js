e76fea1b66a59fcd909a9d6740de865061fca65e
prakash-kumar592 <prakash.kumar989810@gmail.com>
2026-06-27 04:26:52 +0000

    loop

---
+++ 05_iterations/four.js
@@ -0,0 +1,26 @@
+const myObject = {
+    js: 'javascript',
+    cpp: 'C++',
+    rb: "ruby",
+    swift: "swift by apple"
+}
+
+for (const key in myObject) {
+    //console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+const programming = ["js", "rb", "py", "java", "cpp"]//for in loop keys ko print kr deta hai
+
+for (const key in programming) {
+    //console.log(programming[key]);
+}
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('Fr', "France")
+// map.set('IN', "India")
+
+// for (const key in map) {//cant be iteratable on map by using for in loop
+//     console.log(key);
+// }
